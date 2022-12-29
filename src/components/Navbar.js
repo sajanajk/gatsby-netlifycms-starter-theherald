b4bcc6c6c32e121ee686ace7c7f9b449612d294f
sajanajk <120516209+sajanajk@users.noreply.github.com>
2022-12-29 22:37:57 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,9 +41,9 @@ const Navbar = class extends React.Component {
         <div className="container flex-around">
           <div className="navbar-brand logo">
             <Link to="/" className="navbar-item" title="Logo">
-              <h1>Brand | Logo</h1>
+              <h1>The Herald</h1>
             </Link>
-              <GitHubButton href="https://github.com/simarmannsingh">Follow</GitHubButton>
+              
             {/* Hamburger menu */}
             <div
               className={`navbar-burger burger ${this.state.navBarActiveClass}`}
